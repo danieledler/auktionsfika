cd376d558b696bedf3b831d05ff0087d517d8f89
Daniel Edler <edler.dev@gmail.com>
2023-07-04 11:00:01 +0200

    Remove ColorModeScript

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -3,14 +3,14 @@
 
 import { CacheProvider } from "@chakra-ui/next-js";
 import { ChakraProvider } from "@chakra-ui/react";
-import { ColorModeScript } from "@chakra-ui/react";
+// import { ColorModeScript } from "@chakra-ui/react";
 import React from "react";
 import theme from "./theme";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <React.Fragment>
-      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+      {/* <ColorModeScript initialColorMode={theme.config.initialColorMode} /> */}
       <CacheProvider>
         <ChakraProvider theme={theme}>{children}</ChakraProvider>
       </CacheProvider>
